77b2284d64ba73f1b41c735b7451c1add9377a35
qq2953825477 <2953825477@qq.com>
2014-10-30 14:46:21 +0800

    修改了按钮的坐标

--- TestA/TestA/ViewController.m
+++ TestA/TestA/ViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     UIButton *aButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    [aButton setFrame:CGRectMake(10, 10, 30, 24)];
+    [aButton setFrame:CGRectMake(10, 100, 80, 24)];
     [aButton setBackgroundColor:[UIColor blueColor]];
     [aButton setTitle:@"按钮" forState:UIControlStateNormal];
     [aButton addTarget:self action:@selector(buttonAction) forControlEvents:UIControlEventTouchUpInside];
