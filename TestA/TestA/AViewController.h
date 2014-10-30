a3c649d7f4938a4e3dc79cdf227edc7926c7a8f3
qq2953825477 <2953825477@qq.com>
2014-10-30 15:04:13 +0800

    添加了新文件

---
+++ TestA/TestA/AViewController.h
@@ -0,0 +1,13 @@
+//
+//  AViewController.h
+//  TestA
+//
+//  Created by Lixinjie on 14/10/30.
+//  Copyright (c) 2014年 Lixinjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AViewController : UIViewController
+
+@end
--- TestA/TestA/AViewController.m
+++ TestA/TestA/AViewController.m
@@ -1,38 +1,48 @@
 //
-//  ViewController.m
+//  AViewController.m
 //  TestA
 //
 //  Created by Lixinjie on 14/10/30.
 //  Copyright (c) 2014年 Lixinjie. All rights reserved.
 //
 
-#import "ViewController.h"
 #import "AViewController.h"
 
-@interface ViewController ()
+@interface AViewController ()
 
 @end
 
-@implementation ViewController
+@implementation AViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor yellowColor];
     UIButton *aButton = [UIButton buttonWithType:UIButtonTypeCustom];
     [aButton setFrame:CGRectMake(10, 100, 80, 24)];
     [aButton setBackgroundColor:[UIColor blueColor]];
-    [aButton setTitle:@"按钮" forState:UIControlStateNormal];
+    [aButton setTitle:@"返回" forState:UIControlStateNormal];
     [aButton addTarget:self action:@selector(buttonAction) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:aButton];
 }
 - (void)buttonAction {
-    NSLog(@"点击了按钮,self.frame：%@", NSStringFromCGRect(self.view.frame));
-    AViewController *aVC = [[AViewController alloc] init];
-    [self presentViewController:aVC animated:YES completion:nil];
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
+
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
